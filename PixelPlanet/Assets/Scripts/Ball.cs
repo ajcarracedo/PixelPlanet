595bb2e4f4152409ca615ee12a07275f75d1c66b
alberto <ajcarracedo@gmail.com>
2017-02-28 18:10:02 +0100

    Initial commit

---
+++ PixelPlanet/Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    Vector3 newPosition;
+    private Brick brick;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        brick = GameObject.FindObjectOfType<Brick>();
+        transform.position = Vector3.MoveTowards(transform.position, brick.transform.position, 0.02f);
+    }
+}
---
+++ PixelPlanet/Assets/Scripts/Brick.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour
+{
+
+    public AudioClip crack;
+    public Sprite[] hitSprites;
+    public static int breakableCount = 0;
+    public GameObject smoke;
+
+    private int timesHit;
+    private LevelManager levelManager;
+    private bool isBreakable;
+
+    private Ball ball;
+
+    // Use this for initialization
+    void Start()
+    {
+        isBreakable = (this.tag == "Breakable");
+        // Keep track of breakable bricks
+        if (isBreakable)
+        {
+            breakableCount++;
+        }
+
+        timesHit = 0;
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+    }
+
+    void addBall()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 newPosition = ray.origin;
+        ball = GameObject.FindObjectOfType<Ball>();
+        Ball ballClone = (Ball)Instantiate(ball, newPosition, Quaternion.identity);
+        ballClone.GetComponent<Ball>().enabled = true;
+        ballClone.name = "Ball";
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            addBall();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Destroy(col.gameObject);
+
+        if (isBreakable)
+        {
+            HandleHits();
+        }
+    }
+
+    void HandleHits()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            breakableCount--;
+            levelManager.BrickDestoyed();
+            PuffSmoke();
+            Destroy(gameObject);
+        }
+        else
+        {
+            LoadSprites();
+        }
+    }
+
+    void PuffSmoke()
+    {
+        GameObject smokePuff = Instantiate(smoke, transform.position, Quaternion.identity) as GameObject;
+        smokePuff.GetComponent<ParticleSystem>().startColor = gameObject.GetComponent<SpriteRenderer>().color;
+    }
+
+    void LoadSprites()
+    {
+        int spriteIndex = timesHit - 1;
+
+        if (hitSprites[spriteIndex] != null)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Brick sprite missing");
+        }
+    }
+
+    // TODO Remove this method once we can actually win!
+    void SimulateWin()
+    {
+        levelManager.LoadNextLevel();
+    }
+}
